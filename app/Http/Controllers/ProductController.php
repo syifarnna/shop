effc54f4f5dc55df79095707d523e7ec3ebe0a07
syifarnna <syifarn279@gmail.com>
2026-06-09 09:21:02 +0700

    feat: mengubah isi productcontroller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+//import model product
+use App\Models\Product; 
+
+//import return type View
+use Illuminate\View\View;
 
 class ProductController extends Controller
 {
-    //
+    /**
+     * index
+     *
+     * @return void
+     */
+    public function index() : View
+    {
+        //get all products
+        $products = Product::latest()->paginate(10);
+
+        //render view with products
+        return view('products.index', compact('products'));
+    }
 }
