f8c50b23aa09875b800985d3577f6ac96bd9b5fa
syifarnna <syifarn279@gmail.com>
2026-06-09 09:15:10 +0700

    feat: mengubah kolom tabel migration

--- database/migrations/2026_06_09_021338_create_products_table.php
+++ database/migrations/2026_06_09_021338_create_products_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('image');
+            $table->string('title');
+            $table->text('description');
+            $table->bigInteger('price');
+            $table->integer('stock')->default(0);
             $table->timestamps();
         });
     }
