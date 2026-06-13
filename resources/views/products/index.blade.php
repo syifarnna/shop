0f777dbfbb2d2f3ab0aed9d07130f62661319039
syifarnna <syifarn279@gmail.com>
2026-06-13 14:32:21 +0700

    ganti index

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Data Products - SantriKoding.com</title>
+    <title>Data Products</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body style="background: lightgray">
@@ -13,9 +13,7 @@
         <div class="row">
             <div class="col-md-12">
                 <div>
-                    <h3 class="text-center my-4">Tutorial Laravel 12 untuk Pemula</h3>
-                    <h5 class="text-center"><a href="https://santrikoding.com">www.santrikoding.com</a></h5>
-                    <hr>
+                    <h3 class="text-center my-4">Tutorial Laravel 12 untuk Pemula</h3>                    <hr>
                 </div>
                 <div class="card border-0 shadow-sm rounded">
                     <div class="card-body">
--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\ProductController; // Jangan lupa panggil ProductController-nya
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome'); 
 });
 
-// --- RUTE AUTH (LOGIN & REGISTER) ---
+// --- RUTE AUTH (LOGIN, REGISTER, LOGOUT) ---
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
@@ -16,11 +16,11 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 // --- RUTE DASHBOARD ---
+// Dilindungi middleware 'auth' (hanya untuk yang sudah login)
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware('auth');
 
 // --- RUTE PRODUCTS (RESOURCE) ---
-// Gunakan Route::resource agar index, create, store, edit, update, dan destroy otomatis terhubung!
-// Cukup tambahkan ->middleware('auth') di belakangnya
+// Dilindungi middleware 'auth' agar tamu tidak bisa melihat/mengedit produk
 Route::resource('/products', ProductController::class)->middleware('auth');
