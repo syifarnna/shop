04c0c9f7fddcd8edce464db2cd2e2c3b34409189
syifarnna <syifarn279@gmail.com>
2026-06-13 13:57:52 +0700

    feat: rute

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,12 @@
-<?php
-
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-//import product controller
-use App\Http\Controllers\ProductController;
+// Rute untuk Login & Logout
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-//route resource for products
-Route::resource('/products', ProductController::class);
-
-Route::get('/', function () {
-    return view('welcome');
-});
+// Contoh halaman yang hanya bisa diakses kalau SUDAH LOGIN (dilindungi middleware 'auth')
+Route::get('/dashboard', function () {
+    return 'Selamat datang di Dashboard! Ini halaman rahasia.';
+})->middleware('auth');
